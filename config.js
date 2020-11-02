b5f9e0b5ffe325a785fd226dbfdf4ed2cc01e9ad
Piyush <pkkapoor98@gmail.com>
2020-11-02 14:09:47 +0530

    fixed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config={
-     localhost_url:'http://localhost:8080',
+     localhost_url:'http://192.168.1.5:8080',
     // localhost_url:'http://ec2-13-232-135-156.ap-south-1.compute.amazonaws.com:8080'
 
     }
