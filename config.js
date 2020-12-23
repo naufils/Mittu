f7df3c92f842eb2f5bc1972492ad7829d415dd1a
Naufil <48688134+Nabajyoti05@users.noreply.github.com>
2020-12-23 12:11:52 +0530

    Local changes
    Gradle version changes

--- config.js
+++ config.js
@@ -1,7 +1,12 @@
 const config={
-     localhost_url:'http://192.168.1.5:8080',
-    // localhost_url:'http://ec2-13-232-135-156.ap-south-1.compute.amazonaws.com:8080'
+     //after hosting the backend code from the backend app(backend code) into an ec2 instance on aws you need to get the ec2 public link to access that server and replace
+     //the localhost_url variable down here to that new ec2 public access link 
+     
+     // using this localhost_url your frontend app is communicating with the backend, OKAY?? 
 
+     localhost_url:'http://192.168.2.7:8080',
+
+//     localhost_url:'http://ec2-35-154-155-123.ap-south-1.compute.amazonaws.com:8080'   //this one is for hometheatre and it's not having payment and 2 videos algo support
     }
 
     export default config;
